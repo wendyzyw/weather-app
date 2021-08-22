4321aa0303a8a6527643b0dfc9c7f936452efeb3
Wendy Zeng <cocozyw2811@gmail.com>
2021-08-22 13:34:46 +1000

    Set up src structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from '@react-navigation/native';
+import Welcome from "./scenes/welcome";
+import Dashboard from "./scenes/dashboard";
+
+const Stack = createStackNavigator();
 
 const App = () => (
-    <View>
-        <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+    </NavigationContainer>
 );
 
 export default App;
---
+++ src/scenes/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, TouchableHighlight} from 'react-native';
+
+const Dashboard = ({navigation}) => (
+    <SafeAreaView>
+        <Text>Screen: Login</Text>
+
+        <TouchableHighlight>
+            <Text>dashboard</Text>
+        </TouchableHighlight>
+    </SafeAreaView>
+);
+
+export default Dashboard;

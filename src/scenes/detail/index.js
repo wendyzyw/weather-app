c7104e75755ca81f0351b46547acab3b687041a8
Wendy Zeng <cocozyw2811@gmail.com>
2021-08-25 12:22:13 +1000

    Add styling for details

--- src/scenes/detail/index.js
+++ src/scenes/detail/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button, SafeAreaView, ScrollView, ImageBackground} from 'react-native';
+import {View, StyleSheet, SafeAreaView, ScrollView, ImageBackground, TouchableOpacity} from 'react-native';
 import config from "../../assets/config.json";
 import {Card} from "react-native-elements";
 
@@ -55,23 +55,35 @@ const Detail = ({ route, navigation }) => {
         <SafeAreaView>
             <ScrollView>
                 <View>
-                    <Card>
-                        <Card.Title>{weatherData.profile.description}</Card.Title>
-                    </Card>
-                    <View>
-                        <Card>
-                            <Card.Title>{weatherData.measurements.wind}</Card.Title>
+                    <TouchableOpacity>
+                        <Card containerStyle={{flex: 1,
+                            backgroundColor: 'rgba(255, 255, 255, 0.4)',
+                            marginVertical: 10,
+                            padding: 25,
+                            elevation: 0,
+                            borderRadius: 15,
+                            borderWidth: 0}}>
+                            <Card.Title>{weatherData.profile.description}</Card.Title>
                         </Card>
-                        <Card>
-                            <Card.Title>{weatherData.measurements.humidity}</Card.Title>
+                    </TouchableOpacity>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <Card containerStyle={styles.cardContainerLeftStyle} >
+                            <Card.Title>Wind</Card.Title>
+                            <Card.FeaturedSubtitle style={{alignSelf: 'center'}}>{weatherData.measurements.wind}</Card.FeaturedSubtitle>
+                        </Card>
+                        <Card containerStyle={styles.cardContainerRightStyle}>
+                            <Card.Title>Humidity</Card.Title>
+                            <Card.FeaturedSubtitle style={{alignSelf: 'center'}}>{weatherData.measurements.humidity}</Card.FeaturedSubtitle>
                         </Card>
                     </View>
-                    <View>
-                        <Card>
-                            <Card.Title>{weatherData.measurements.min}</Card.Title>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <Card containerStyle={styles.cardContainerLeftStyle}>
+                            <Card.Title>Min temperature</Card.Title>
+                            <Card.FeaturedSubtitle style={{alignSelf: 'center'}}>{weatherData.measurements.min}</Card.FeaturedSubtitle>
                         </Card>
-                        <Card>
-                            <Card.Title>{weatherData.measurements.max}</Card.Title>
+                        <Card containerStyle={styles.cardContainerRightStyle}>
+                            <Card.Title>Max temperature</Card.Title>
+                            <Card.FeaturedSubtitle style={{alignSelf: 'center'}}>{weatherData.measurements.max}</Card.FeaturedSubtitle>
                         </Card>
                     </View>
                 </View>
@@ -80,5 +92,28 @@ const Detail = ({ route, navigation }) => {
         </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    cardContainerLeftStyle: {
+        flex: 1,
+        marginRight: 7.5,
+        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        marginVertical: 10,
+        padding: 25,
+        elevation: 0,
+        borderRadius: 15,
+        borderWidth: 0
+    },
+    cardContainerRightStyle: {
+        flex: 1,
+        marginLeft: 7.5,
+        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        marginVertical: 10,
+        padding: 25,
+        elevation: 0,
+        borderRadius: 15,
+        borderWidth: 0
+    },
+});
 
 export default Detail;
